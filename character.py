7876b7896910498a63aaa5a9b19d6cbef73f2edf
jbucanay <gilbert.baracka@gmail.com>
2021-09-19 13:13:11 -0600

    structure for player and character set

---
+++ character.py
@@ -0,0 +1,24 @@
+## get characters from avengers api
+
+class Character:
+    def __init__(self, name, description, story):
+        self.name = name
+        self.description = description
+        self.story = story
+
+        def setname(self):
+            pass
+        def getname(self):
+            pass
+        def setdescription():
+            pass
+        def getdescription():
+            pass
+        def setstory():
+            pass
+
+        def apiCall():
+            pass
+
+
+
---
+++ player.py
@@ -0,0 +1,12 @@
+from character import Character
+
+class Player(Character):
+    def __init__(self, playername):
+        super().__init__(self)
+        self.playername = playername
+
+
+        def getname():
+            pass
+        def setname():
+            pass
